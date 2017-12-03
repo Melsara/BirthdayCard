7a5fa798b3cbadf399f1f1cfc6be888c40bb72af
Melsara <kaxhortz@gmail.com>
2017-12-03 18:51:52 +0100

    Importing everything to repo.

---
+++ app/src/main/java/com/sardegnaisoladicavalli/example/happybirthdaydaddie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sardegnaisoladicavalli.example.happybirthdaydaddie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
